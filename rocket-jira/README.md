ebc46e27fd152c96bb0b3a5c7acf12c08e3b2e2b
Andrey Kartashov <deftbrain@gmail.com>
2021-03-16 17:02:45 +0200

    feat(Rocket Jira): add an ability to set a custom select field on a created subtask

--- rocket-jira/README.md
+++ rocket-jira/README.md
@@ -1,33 +1,42 @@
 # Rocket Jira
-A [userscript](https://openuserjs.org/about/Userscript-Beginners-HOWTO) that
-helps to automate some chore in Jira. The only feature for now is creating
-many subtasks with estimate and assignee for a Jira issue from a context.
+A [userscript](https://openuserjs.org/about/Userscript-Beginners-HOWTO) helps to
+automate some chore in Jira. The only feature for now is creating many subtasks
+(with specified estimate, assignee, and one custom select field) for a Jira issue
+from a context.
 
 ## Requirements
 * Chrome 76+ / Firefox 71+ / Safari 13+ / Edge 79+
 * [Tampermonkey](http://www.tampermonkey.net/) browser extension
-* JIRA 7.0+ instance (by default the script is enabled on the 'https://**.atlassian.net/*'
+* [JIRA 7.0+](https://docs.atlassian.com/software/jira/docs/api/REST/7.0.0/) instance (by default the script is enabled on the 'https://**.atlassian.net/*'
  pages, but you can change that criteria via script settings)
 * You have to be authorized in your Jira instance to let the script send authorized requests on your behalf
 
 ## Installation
-1. Open the [userscript](https://github.com/deftbrain/userscripts/raw/main/rocket-jira/rocket-jira.user.js) in a browser.
- _Tampermonkey will pick up the file automatically and ask you to install it_;
+1. Open the [userscript](https://github.com/deftbrain/userscripts/raw/main/rocket-jira/rocket-jira.user.js) in a browser;  
+ _Tampermonkey will pick up the file automatically and ask you to install it._
 1. Click the `Install` button;
 1. Open any page of your Jira instance in the browser;
-1. Staying in the same tab, click the `Tampermonkey` extension icon
- -> `Rocket Jira` -> `Preferences...`;
+1. Staying in the same tab, click the Tampermonkey extension icon -> `Rocket Jira` -> `Preferences...`;
 1. Select a Jira project you work on in the `Project` dropdown;
-1. Click the `Save` button in the modal window and wait for a few seconds until the window appears again.
- _The new option `Subtask Issue Type` will appear in the modal window after that action_;
+1. Click the `Save` button in the modal window and wait for a few seconds until the window appears again
+ (sorry for that annoying way, we already have an [issue for improvement](https://github.com/deftbrain/userscripts/issues/1));  
+ _The new option `Subtask Issue Type` will appear in the modal window after that action._
 1. Select an issue type for subtasks you are going to create in the future;
 1. Click the `Save` button;
+1. If you need to set some custom field (currently only the `select` field is supported):
+    1. Select a field name using the `Custom Select Field Name` dropdown and click the `Save` button;
+    1. Select a field value using the `Custom Select Field Value` dropdown and click the `Save` button.
 1. Click the `Close` button.
 
 ## Usage
-1. Open an issue in Jira (from a board or in a separate tab);
-1. Staying in the same tab, click the `Tampermonkey` extension icon
- -> `Rocket Jira` -> `Create subtasks...`;
-1. Enter the subtask name and estimate in Jira format (e.g. `1h 30m`) dividing them by slash `/`.
- You can specify multiple name-estimate pairs dividing them by comma `,`;
+1. Open an issue you would like to add subtasks to in Jira (from a board or in a separate tab, both ways are supported);
+1. Staying in the same tab, click the Tampermonkey extension icon -> `Rocket Jira` -> `Create subtasks...`;
+1. Enter the subtask name and estimate in Jira format (e.g. `1h 30m`) separating them with a slash (`/`).
+ You can specify multiple name-estimate pairs separating them with a comma (`,`);
 1. Wait for the alert with the status of the operation.
+
+## Getting updates
+By default, Tampermonkey checks for userscript updates every day.
+It will show you a notification in a browser once an update is available.
+Click the notification to see update details and install it if you are ready.
+You can check for updates manually clicking the Tampermonkey extension icon -> `Check for userscript updates`.
--- rocket-jira/rocket-jira.user.js
+++ rocket-jira/rocket-jira.user.js
@@ -1,9 +1,8 @@
 // ==UserScript==
 // @name         Rocket Jira
 // @homepage     https://github.com/deftbrain/userscripts/tree/main/rocket-jira
-// @version      1.0
-// @description  A userscript that helps to automate some chore in Jira.
-//               The only feature for now is creating many subtasks with estimate and assignee for a Jira issue from a context.
+// @version      1.1
+// @description  A userscript that helps to automate some chore in Jira. The only feature for now is creating many subtasks (with specified estimate, assignee, and one custom select field) for a Jira issue from a context.
 // @author       https://github.com/deftbrain
 // @match        https://*.atlassian.net/*
 // @require      https://openuserjs.org/src/libs/sizzle/GM_config.min.js
@@ -19,12 +18,17 @@
 	const CONFIG_ID = 'main';
 	const CONFIG_FIELD_PROJECT = 'project';
 	const CONFIG_FIELD_SUBTASK_ISSUE_TYPE = 'subtaskIssueType';
+	const CONFIG_FIELD_CUSTOM_SELECT_FIELD_NAME_1 = 'customSelectFieldName1';
+	const CONFIG_FIELD_CUSTOM_SELECT_FIELD_VALUE_1 = 'customSelectFieldValue1';
 	const CONFIG_FIELDS = {
-		[CONFIG_FIELD_PROJECT]: 'Project',
-		[CONFIG_FIELD_SUBTASK_ISSUE_TYPE]: 'Subtask Issue Type',
+		[CONFIG_FIELD_PROJECT]: 'Project*',
+		[CONFIG_FIELD_SUBTASK_ISSUE_TYPE]: 'Subtask Issue Type*',
+		[CONFIG_FIELD_CUSTOM_SELECT_FIELD_NAME_1]: 'Custom Select Field Name',
+		[CONFIG_FIELD_CUSTOM_SELECT_FIELD_VALUE_1]: 'Custom Select Field Value',
 	};
 	const CONFIG_JIRA_ENTITY_NAME_ID_DELIMITER = ' @ ';
 	const JIRA_API_BASE_URL = `${window.location.protocol}//${window.location.hostname}/rest/api/2`;
+	const JIRA_CUSTOM_SELECT_FIELD_SCHEMA = 'com.atlassian.jira.plugin.system.customfieldtypes:select';
 
 	setupMenu();
 
@@ -34,9 +38,9 @@
 	}
 
 	function createSubtasks() {
-		for (const field in CONFIG_FIELDS) {
-			if (!getRawConfigValue(field)) {
-				alert('Required fields are not set in the preferences of the userscript!');
+		for (const field of [CONFIG_FIELD_PROJECT, CONFIG_FIELD_SUBTASK_ISSUE_TYPE]) {
+			if (!getValueFromConfig(field)) {
+				alert(`The required field '${CONFIG_FIELDS[field]}' is not set in the preferences of the userscript!`);
 				return;
 			}
 		}
@@ -49,33 +53,35 @@
 
 		const subtaskEstimateMap = {};
 		for (const nameAndEstimate of subtasks.split(',')) {
-			const [summary, estimate] = nameAndEstimate.trim().split('/');
-			subtaskEstimateMap[summary] = estimate.trim();
+			let [summary, estimate] = nameAndEstimate.trim().split('/');
+			summary = summary && summary.trim();
+			estimate = estimate && estimate.trim();
+			if (!summary || !estimate) {
+				alert(`Invalid input. Please copy the input, correct it and try again. Input: ${subtasks}`);
+				return;
+			}
+			subtaskEstimateMap[summary] = estimate;
 		}
-		const project = getConfigValue(CONFIG_FIELD_PROJECT);
-		const issueType = getConfigValue(CONFIG_FIELD_SUBTASK_ISSUE_TYPE);
 		return sendRequest(JIRA_API_BASE_URL + '/myself')
 			.then(data => {
 				const issues = [];
+				const commonFields = {
+					assignee: {accountId: data.accountId},
+					issuetype: {id: getIdFromConfig(CONFIG_FIELD_SUBTASK_ISSUE_TYPE)},
+					parent: {key: parentIssueId},
+					project: {id: getIdFromConfig(CONFIG_FIELD_PROJECT)},
+				};
+				const customSelectFieldName = getIdFromConfig(CONFIG_FIELD_CUSTOM_SELECT_FIELD_NAME_1);
+				const customSelectFieldValue = getValueFromConfig(CONFIG_FIELD_CUSTOM_SELECT_FIELD_VALUE_1);
+				if (customSelectFieldName && customSelectFieldValue) {
+					commonFields[customSelectFieldName] = {value: customSelectFieldValue};
+				}
 				for (const [summary, estimate] of Object.entries(subtaskEstimateMap)) {
 					issues.push({
 						fields: {
-							project: {
-								id: project
-							},
-							parent: {
-								key: parentIssueId
-							},
+							...commonFields,
 							summary: summary,
-							issuetype: {
-								id: issueType
-							},
-							assignee: {
-								accountId: data.accountId
-							},
-							timetracking: {
-								originalEstimate: estimate
-							}
+							timetracking: {originalEstimate: estimate},
 						}
 					});
 				}
@@ -131,9 +137,29 @@
 				alert(`Unable to fetch a list of subtask issue types`);
 				throw error;
 			}).then(data => {
-				return data.projects[0].issuetypes
+				const map = data.projects[0].issuetypes
 					.filter(issueType => issueType.subtask)
 					.map(issueType => `${issueType.name}${CONFIG_JIRA_ENTITY_NAME_ID_DELIMITER}${issueType.id}`);
+				map.sort();
+				return map;
+			});
+	}
+
+	async function getIssueCustomSelectFields(projectId, issueTypeId) {
+		return sendRequest(JIRA_API_BASE_URL + `/issue/createmeta?projectIds=${projectId}&issuetypeIds=${issueTypeId}&expand=projects.issuetypes.fields`)
+			.catch(error => {
+				alert(`Unable to fetch custom fields`);
+				throw error;
+			}).then(data => {
+				const map = {};
+				const fields = Object.values(data.projects[0].issuetypes[0].fields)
+					.filter(field => field.schema.custom === JIRA_CUSTOM_SELECT_FIELD_SCHEMA);
+				for (const field of fields) {
+					const values = field.allowedValues.map(props => props.value);
+					values.sort();
+					map[field.name + CONFIG_JIRA_ENTITY_NAME_ID_DELIMITER + field.key] = values;
+				}
+				return map;
 			});
 	}
 
@@ -170,7 +196,7 @@
 	}
 
 	function getProject() {
-		return getConfigValue(CONFIG_FIELD_PROJECT);
+		return getIdFromConfig(CONFIG_FIELD_PROJECT);
 	}
 
 	function prepareConfig() {
@@ -187,8 +213,8 @@
 					},
 					init: function() {
 						for (const field in CONFIG_FIELDS) {
-							if (GM_config.fields[field] && getRawConfigValue(field)) {
-								GM_config.fields[field].value = getRawConfigValue(field);
+							if (GM_config.fields[field] && getValueFromConfig(field)) {
+								GM_config.fields[field].value = getValueFromConfig(field);
 							}
 						}
 					},
@@ -207,8 +233,8 @@
 		});
 	}
 
-	function getConfigValue(field) {
-		const value = getRawConfigValue(field);
+	function getIdFromConfig(field) {
+		const value = getValueFromConfig(field);
 		if (value) {
 			return value.split(CONFIG_JIRA_ENTITY_NAME_ID_DELIMITER)[1];
 		}
@@ -216,29 +242,48 @@
 		return value;
 	}
 
-	function getRawConfigValue(field) {
+	function getValueFromConfig(field) {
 		return GM_config.getValue(field) || null;
 	}
 
 	async function getFields() {
 		const fields = {};
-		const project = getProject();
+		let customFields;
+		const project = getIdFromConfig(CONFIG_FIELD_PROJECT);
 		for (const field in CONFIG_FIELDS) {
-			if (!project && field !== CONFIG_FIELD_PROJECT) {
-				// Hide a project-dependent field from the config if the project is not set
-				fields[field] = null;
-				continue;
+			let options;
+			switch (field) {
+				case CONFIG_FIELD_PROJECT:
+					options = getProjects();
+					break;
+				case CONFIG_FIELD_SUBTASK_ISSUE_TYPE:
+					if (project) {
+						options = getSubtaskIssueTypes(project);
+						break;
+					}
+				case CONFIG_FIELD_CUSTOM_SELECT_FIELD_NAME_1:
+					const subtaskIssueType = getIdFromConfig(CONFIG_FIELD_SUBTASK_ISSUE_TYPE);
+					if (project && subtaskIssueType) {
+						customFields = await getIssueCustomSelectFields(project, subtaskIssueType);
+						options = Object.keys(customFields);
+						break;
+					}
+				case CONFIG_FIELD_CUSTOM_SELECT_FIELD_VALUE_1:
+					const customSelectFieldNameRaw = getValueFromConfig(CONFIG_FIELD_CUSTOM_SELECT_FIELD_NAME_1);
+					if (customFields && customSelectFieldNameRaw) {
+						options = Object.values(customFields[customSelectFieldNameRaw]);
+						break;
+					}
+				default:
+					// Hide a project-dependent field from the config if the project is not set
+					fields[field] = null;
+					continue;
 			}
-
-			// Hardcode getting options since we have to few fields
-			const options = field === CONFIG_FIELD_PROJECT
-				? await getProjects()
-				: await getSubtaskIssueTypes(project);
 
 			fields[field] = {
 				label: CONFIG_FIELDS[field],
 				type: 'select',
-				options: options,
+				options: ['', ...(await options)],
 				save: false,
 			};
 		}
